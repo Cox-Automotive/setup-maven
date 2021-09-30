c7f6d626aa3d960076de1dcc2f530039457743a6
Kerry Hatcher <kerry.hatcher@coxautoinc.com>
2021-09-30 16:38:26 -0400

    Automated code formatting changes

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -8,7 +8,6 @@ jest.mock('../src/main', () => ({
 
 import {getMaven} from '../src/main'
 
-
 const cachePath = path.join(__dirname, 'CACHE')
 const tempPath = path.join(__dirname, 'TEMP')
 
@@ -18,36 +17,41 @@ process.env['RUNNER_TOOL_CACHE'] = cachePath
 
 describe('Download Maven Version', () => {
   test('with invalid string', async () => {
-    (getMaven as jest.Mock).mockImplementation(() => { throw new Error("invalid")})
-    const input = "invalid.version.input";
+    ;(getMaven as jest.Mock).mockImplementation(() => {
+      throw new Error('invalid')
+    })
+    const input = 'invalid.version.input'
 
     expect(() => {
       getMaven(input)
-    }).toThrow();
-
+    }).toThrow()
   })
 
   test('with invalid symver', async () => {
-    (getMaven as jest.Mock).mockImplementation(() => { throw new Error("invalid")})
-    const input = "382"
+    ;(getMaven as jest.Mock).mockImplementation(() => {
+      throw new Error('invalid')
+    })
+    const input = '382'
 
-    expect( () => {
+    expect(() => {
       getMaven(input)
     }).toThrow()
   })
 
   test('with non-existent version', async () => {
-    (getMaven as jest.Mock).mockImplementation(() => { throw new Error("does not exist")})
-    const input = "100.1.2"
+    ;(getMaven as jest.Mock).mockImplementation(() => {
+      throw new Error('does not exist')
+    })
+    const input = '100.1.2'
 
     expect(() => {
       getMaven(input)
-    }).toThrow();
+    }).toThrow()
   })
 
   test('with a valid version', async () => {
-    (getMaven as jest.Mock).mockImplementation(() => "Download Success")
-    const input = "3.0.5"
+    ;(getMaven as jest.Mock).mockImplementation(() => 'Download Success')
+    const input = '3.0.5'
 
     expect(() => {
       getMaven(input)
