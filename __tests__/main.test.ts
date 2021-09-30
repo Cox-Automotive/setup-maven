950a5e5afeb5bb75d5810ffe6f597afc17612101
Andrew Magana <andrew.magana@coxautoinc.com>
2021-09-30 10:42:51 -0400

    Fixed jest imports.

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,6 +1,13 @@
-import {getMaven} from '../src/main'
 import {jest, describe, expect, test} from '@jest/globals'
 import * as path from 'path'
+
+jest.mock('../src/main', () => ({
+  __esModule: true,
+  getMaven: jest.fn()
+}))
+
+import {getMaven} from '../src/main'
+import exp = require('constants')
 
 const cachePath = path.join(__dirname, 'CACHE')
 const tempPath = path.join(__dirname, 'TEMP')
@@ -11,17 +18,24 @@ process.env['RUNNER_TOOL_CACHE'] = cachePath
 
 describe('Download Maven Version', () => {
   test('with invalid input', async () => {
-    const input = 'invalid.version.number'
-    await expect(getMaven(input)).rejects.toThrow('invalid version input')
+
+    (getMaven as jest.Mock).mockImplementation(() => console.log("mocked getMaven"))
+    await getMaven('invalid.version.input')
   })
+
+  test('with invalid string', async () => {
+    const input = 382
+    await expect(getMaven(String(input))).rejects.toThrow()
+  })
+
+  test('with non-existent version', async () => {
+    const input = '100.1.2'
+    await expect(getMaven(input)).rejects.toThrow()
+  })
+
+  // test('with a valid version', async () => {
   //
-  // test('with invalid string', async () => {
-  //   const input = 382
-  //   await expect(getMaven(String(input))).rejects.toThrow()
-  // })
-  //
-  // test('with non-existent version', async () => {
-  //   const input = '100.1.2'
-  //   await expect(getMaven(input)).rejects.toThrow()
+  //   const input = '3.0.5'
+  //   await expect(getMaven(input)).resolves.toBeTruthy()
   // })
 })
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
-  testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
   verbose: true
 }
